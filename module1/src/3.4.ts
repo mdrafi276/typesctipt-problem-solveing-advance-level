c5a569028d873620b0cbcbea135e4b3cefc510e4
mdrafi276 <mdrafi37473@gmail.com>
2024-05-05 11:55:23 +0600

    complete 3.4.ts class

--- module1/src/3.4.ts
+++ module1/src/3.4.ts
@@ -2,6 +2,62 @@
     // instanceof guard
     class Animal {
         name: string;
-        species: string
+        species: string;
+
+        constructor(name:string, species:string){
+            this.name = name;
+            this.species = species;
+        }
+        makeSound(){
+            console.log("I am making sound");
+        }
     }
+
+    class Dog extends Animal{
+        constructor(name: string, epecies: string){
+            super(name, epecies )
+        }
+        makeBark(){
+            console.log("I am barking");
+        }
+    }
+
+        class Cat extends Animal{
+            constructor(name: string, epecies: string){
+                super(name, epecies )
+            }
+            makeMeaw(){
+                console.log("I am making");
+            }
+
+
+        }
+
+        //
+        const isDog = (animal:Animal):animal is Dog{
+            return animal instanceof Dog
+        }
+        const isCat = (animal:Animal): animal is Cat{
+            return animal instanceof Dog
+        }
+
+        const getAnimal = (animal: Animal){
+            if(isDog(animal)){
+                animal.makeBark();
+            }else if(isCat(animal)){
+                animal.makeMeaw()
+
+            }else{
+                animal.makeSound()
+            }
+        }
+
+        
+        const dog = new Dog("Dog Bhai", 'Dog')
+        
+        const cat = new Cat("Cat Bhai ", "cat")
+        
+        getAnimal(dog)
+        //
+
 }
